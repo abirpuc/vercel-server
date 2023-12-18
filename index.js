24c31dcfb2333850813547f575e7eb9e56408962
Mozzammel Hossen <53122797+abirpuc@users.noreply.github.com>
2023-12-18 14:53:55 +0600

    add post data

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 
 const app = express()
-
+const post = require('./post.json')
 const port = process.env.PORT || 5000
 
 app.use(express.json())
 
 app.get('/',(req, res)=>{
     res.send('Hello the server is running!')
+})
+
+
+app.get('/post', (req, res)=>{
+    res.send(post)
 })
 
 app.listen(port,()=>{
